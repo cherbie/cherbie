b5145c5ccacfcda38e9357d7601a30fb3538a371
Clayton Herbst <herbsca1@gmail.com>
2023-01-09 13:23:31 +1100

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<nav>
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+</nav>
+
+<slot />
+
+<script>
+    import "../app.css";
+</script>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,4 @@
+<article class="prose">
+    <h3>Welcome to SvelteKit</h3>
+    <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+</article>
