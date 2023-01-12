bd8a274c6400307073f0624fe57d2b3ddcc4f469
Clayton Herbst <herbsca1@gmail.com>
2023-01-12 15:46:59 +1100

    Fix. initial render title centering glitch

--- src/lib/components/TopbarContent/TopbarContent.svelte
+++ src/lib/components/TopbarContent/TopbarContent.svelte
@@ -5,14 +5,14 @@
 <div
   class="flex justify-between items-center gap-3 px-6 py-4 font-sans text-gray-700"
 >
-  <div style="width: {topbarButtonsContainerWidth}px" />
+  <div class="w-[84px]" />
   <h1
     class="font-extrabold text-2xl flex-grow flex-shrink basis-auto justify-center items-center text-center whitespace-nowrap overflow-hidden text-ellipsis m-0"
   >
     <slot name="topbar-title">Title</slot>
   </h1>
   <div
-    class="flex flex-shrink-0 gap-2"
+    class="flex flex-shrink-0 gap-2 min-w-[84px]"
     bind:clientWidth={topbarButtonsContainerWidth}
   >
     <slot name="topbar-buttons" />
