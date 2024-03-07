58a291d5426a9200d6ea975bd6deb067bc100203
Clayton Herbst <herbsca1@gmail.com>
2024-03-08 00:29:30 +1100

    fix: add static astro config output option

--- astro.config.ts
+++ astro.config.ts
@@ -21,7 +21,7 @@ export default defineConfig({
   server: {
     port: serverPort,
   },
-  // Enable Svelte to support Svelte components.
+  output: 'static',
   integrations: [
     sitemap(),
     tailwind({
